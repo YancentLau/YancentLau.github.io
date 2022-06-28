178355ce3bf2d04f4f6202745190419a509e4909
YancentLau <20069112d@connect.polyu.hk>
2022-06-28 12:20:43 +0800

    28062022_1220

---
+++ mission1/wk2_1_answer.py
@@ -0,0 +1,17 @@
+agent.move("forward")
+pickUpFuel()
+for i in range(5):
+    agent.move("left")
+pickUpFuel()
+for i in range(10):
+    agent.move("right")
+pickUpFuel()
+for i in range(6):
+    agent.move("forward")
+placeFuel()
+for i in range(5):
+    agent.move("left")
+placeFuel()
+for i in range(5):
+    agent.move("left")
+placeFuel()
